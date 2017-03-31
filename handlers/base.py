8119b73cdaad1fd997d4c1c6190378510408baf9
eloiGarrido <garrido.eloi@gmail.com>
2017-03-31 17:39:54 +0200

    [httpClient] Add client and xml parser
    - Add Async http client to forward nextBus queries
    - Return JSON output out of XML nextBus query

--- handlers/base.py
+++ handlers/base.py
@@ -1,28 +1,50 @@
+from tornado import gen
 import tornado.ioloop
 import tornado.web
-import json
+import tornado.httpserver
+import tornado.httpclient
 from tornado.concurrent import Future
-from tornado import gen
 import logging
+import logging.config
+import settings as settings
+import json
+import xmltodict
 
 # Initialize logger
+logging.config.dictConfig(settings.LOGGING)
 logger = logging.getLogger('nextBus')
 
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        self.write("Welcome to nextBus Enhanced.\n" + "<br>\n")
+        self.write('Request information with the following query structure:\n<br>\n<br>http://localhost:8888/'
+                   'publicJSONFeed''?command=commandName&a=agencyTag&additionParams..')
 
 
 class MessageHandler(tornado.web.RequestHandler):
+    @gen.coroutine
     def get(self):
-        self.write('message')
+
+        # Initialize Next Bus Handler object
+        args = self.request.query
+        url = settings.WEB_ENDPOINT + str(args)
+        client = tornado.httpclient.AsyncHTTPClient()
+        yield client.fetch(url, self.handle_request)
+
+    def handle_request(self, response):
+        if response.error:
+            print("Error:", response.error)
+        else:
+            data = xmltodict.parse(response.body.decode('utf-8'), xml_attribs=True)
+            self.write(data)
+        self.finish()
 
 
 def main():
     app = tornado.web.Application([
         (r"/", MainHandler),
-        (r"/api/message", MessageHandler),
+        (r"/publicJSONFeed", MessageHandler),
     ])
     app.listen(8888)
     tornado.ioloop.IOLoop.current().start()
@@ -30,4 +52,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
